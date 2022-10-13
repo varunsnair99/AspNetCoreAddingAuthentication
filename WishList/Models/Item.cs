38ac3a9a715970ac851a5ed2709ce817b19388b4
Varun S Nair <varunsnair99@gmail.com>
2022-10-13 11:30:56 +0530

    Commit2-Virtual

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -12,6 +12,6 @@ namespace WishList.Models
         public string Description { get; set; }
         public virtual ApplicationUser User { get; set; }
 
-        public virtual ICollection<Item> Items { get; set; }
+      
     }
 }
